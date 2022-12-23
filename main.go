0949a81589f7520216215986e6c57d344454c9f6
nurmeden <nurmeden.02@gmail.com>
2022-12-23 22:45:28 +0600

    first

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"forum/pkg/handler"
 	"log"
 	"net/http"
 )
@@ -8,6 +9,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handler.Home_Page)
+	log.Println("Запуск веб-сервера на http://localhost:8080/ ")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
