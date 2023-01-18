ab1295715b7c8aad287d8db5e0bebaaf9c295364
nurmeden <nurmeden.02@gmail.com>
2023-01-19 02:40:48 +0600

    sdmv;

--- pkg/handler/post.go
+++ pkg/handler/post.go
@@ -1,17 +1,43 @@
 package handler
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 	"text/template"
 )
+
+type post struct {
+	id          int
+	owner       string
+	title       string
+	description string
+	likes       string
+	dislikes    string
+}
 
 func Post(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("./resources/html/post.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = tmpl.Execute(w, nil)
+	db, _ := sql.Open("sqlite3", "./forum.db")
+	rows, err := db.Query("SELECT * FROM post")
+	var id int
+	var owner string
+	var title string
+	var content string
+	var likes int
+	var dislikes int
+	for rows.Next() {
+		err = rows.Scan(&id, &title, &owner, &content, &likes, &dislikes)
+		fmt.Println(id, title, owner, content, likes, dislikes)
+	}
+
+	defer db.Close()
+	err = tmpl.Execute(w, post{})
 	if err != nil {
 		log.Fatal(err)
 	}
